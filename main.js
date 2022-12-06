6700a7f706217d2b6dbb68d84372440e355dc856
unknown <mukasawamukasaevelyne@gmail.com>
2022-12-06 07:40:56 +0200

    slider finished

---
+++ main.js
@@ -0,0 +1,23 @@
+let slideIndex=1;
+showSlides(slideIndex);
+
+let prev =document.getElementById("prev");
+let next =document.getElementById("next");
+
+prev.addEventListener('click',()=>{
+    showSlides(slideIndex += -1);
+})
+next.addEventListener('click',()=>{
+    showSlides(slideIndex += 1);
+})
+
+function showSlides(n){
+    let i;
+    let slides =document.getElementsByClassName('slide');
+    if (n>slides.length){slideIndex=1}
+    if (n<1){slideIndex=slides.length}
+    for(i=0;i<slides.length ; i++){
+        slides[i].style.display="none";
+    }
+    slides[slideIndex-1].style.display="grid";
+}
